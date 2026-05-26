1f93d715f2725344e885c92a826f62a11f19dcb8
Sajjadul Islam <sajjadul625@gmail.com>
2026-05-27 00:39:37 +0600

    Grades of Student

---
+++ 6.  Conditional statements/Grades_of_Student.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int marks;
+    cin >> marks;
+
+    if (marks > 90)
+        cout << "A";
+    else if (marks > 70)
+        cout << "B";
+    else if (marks >= 40)
+        cout << "C";
+    else
+        cout << "F";
+
+    return 0;
+}
