e560eafa31183ff9614201f32ce8dfc7c482898c
Sajjadul Islam <sajjadul625@gmail.com>
2026-07-09 03:01:48 +0600

    Print Factorial

---
+++ 10.Loops/Print_factorial.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    long long factorial = 1;
+    int i = 1;
+
+    do {
+        factorial *= i;
+        i++;
+    } while (i <= n);
+
+    cout << factorial << endl;
+
+    return 0;
+}
